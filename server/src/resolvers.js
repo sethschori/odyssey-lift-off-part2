5290a69f9042137be19428494c86cfc8905dd03b
Seth Schori <20755795+sethschori@users.noreply.github.com>
2022-10-07 19:02:28 -0400

    Flesh out tracksForHome; Add Track resolver

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -2,8 +2,15 @@ const resolvers = {
     Query: {
         // returns an array of Tracks that will be used to populate
         // the homepage grid of our web client
-        tracksForHome: () => {}
-    }
+        tracksForHome: (_, __, {dataSources}) => {
+            return dataSources.TrackAPI.getTracksForHome();
+        },
+    },
+    Track: {
+        author: ({authorId}, _, {dataSources}) => {
+            return dataSources.TrackAPI.getAuthor(authorId);
+        },
+    },
 };
 
 module.exports = resolvers;
