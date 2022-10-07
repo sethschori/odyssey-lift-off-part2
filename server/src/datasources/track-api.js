7d7dae09bc923cc92a9837ab18c61b5fe1e85d55
Seth Schori <20755795+sethschori@users.noreply.github.com>
2022-10-07 16:47:12 -0400

    Add get methods to TrackAPI

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -5,6 +5,15 @@ class TrackAPI extends RESTDataSource {
         super();
         this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com';
     }
+
+    getTracksForHome() {
+        return this.get('tracks');
+    }
+
+    getAuthor(authorId) {
+        return this.get(`author/${encodeURIComponent(authorId)}`);
+    }
 }
+
 
 module.exports = TrackAPI;
