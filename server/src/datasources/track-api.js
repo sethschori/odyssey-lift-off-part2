6c6243584c6d6ce7bc2d288b17a7f245b39cda82
Seth Schori <20755795+sethschori@users.noreply.github.com>
2022-10-07 16:41:48 -0400

    Create TrackAPI class
    TrackAPI extends RESTDataSource and sets baseURL to the Heroku-hosted REST API that we're using.

---
+++ server/src/datasources/track-api.js
@@ -0,0 +1,10 @@
+const {RESTDataSource} = require('apollo-datasource-rest')
+
+class TrackAPI extends RESTDataSource {
+    constructor() {
+        super();
+        this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com';
+    }
+}
+
+module.exports = TrackAPI;
