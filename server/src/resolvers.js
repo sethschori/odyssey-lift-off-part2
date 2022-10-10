933e3a0215abf119469345ac70b04f5c84e3033d
Seth Schori <20755795+sethschori@users.noreply.github.com>
2022-10-10 10:02:07 -0400

    Fix PascalCase => CamelCase

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,12 +3,12 @@ const resolvers = {
         // returns an array of Tracks that will be used to populate
         // the homepage grid of our web client
         tracksForHome: (_, __, {dataSources}) => {
-            return dataSources.TrackAPI.getTracksForHome();
+            return dataSources.trackAPI.getTracksForHome();
         },
     },
     Track: {
         author: ({authorId}, _, {dataSources}) => {
-            return dataSources.TrackAPI.getAuthor(authorId);
+            return dataSources.trackAPI.getAuthor(authorId);
         },
     },
 };
